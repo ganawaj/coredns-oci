f24247ea57727408c65d2e122c5de71c1ef5897b
Ganawa Juanah <ganawa@juanah.com>
2024-06-02 14:43:40 -0500

    basic pulling implemented

--- oci.go
+++ oci.go
@@ -3,7 +3,7 @@ package oci
 import (
 	"context"
 	"fmt"
-	"net/http"
+	// "net/http"
 
 	// "os"
 	"time"
@@ -14,9 +14,10 @@ import (
 	// "oras.land/oras-go/v2/registry"
 	oras "oras.land/oras-go/v2"
 	"oras.land/oras-go/v2/content/file"
+	"oras.land/oras-go/v2/content/memory"
 	"oras.land/oras-go/v2/registry/remote"
-	"oras.land/oras-go/v2/registry/remote/auth"
-	"oras.land/oras-go/v2/registry/remote/retry"
+	// "oras.land/oras-go/v2/registry/remote/auth"
+	// "oras.land/oras-go/v2/registry/remote/retry"
 	// ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 )
 
@@ -40,7 +41,7 @@ type Artifact struct {
 	Path     string
 
 	// file store for the artifact
-	fs 				*file.Store
+	fs *file.Store
 
 	// remote registry
 	remote *remote.Repository
@@ -62,107 +63,100 @@ func (a *Artifact) Pull(c context.Context) error {
 
 	// If the artifact has already been pulled and the interval has not passed, return
 	if time.Since(a.lastPull) < a.Interval {
+		log.Debug("artifact has already been pulled")
 		return nil
 	}
 
 	// Create a new context with a timeout of 10 seconds
-	// TODO: Make this adjustable
-	ctx, cancel := context.WithTimeout(c, 10*time.Second)
+	ctx, cancel := context.WithTimeout(c, 30*time.Second)
 	defer cancel()
 
-	retries := make(chan int)
-	done := make(chan error)
+	retryCount := 0
+	maxRetries := numRetries
 
 	var err error
 
-	// start a goroutine to pull the artifact
-	go a.pull(ctx, retries, done)
-
-	// wait for the artifact to be pulled
 	for {
 		select {
 		case <-ctx.Done():
-			switch ctx.Err() {
 
-			// If the context was canceled or exceeded the deadline, return nil
-			case context.DeadlineExceeded:
-				return fmt.Errorf("failed to pull artifact %v:%v - %v", a.Repository, a.Reference, ctx.Err())
+			// If the context is done, return the context's error
+			return ctx.Err()
 
-			// If the context was canceled, return nil
-			// We don't care about the error since it would be caught by the done channel
-			case context.Canceled:
+		default:
+
+			// Try to pull the artifact
+			err = a.pull(ctx)
+
+			if err == nil {
+
+				// Update the last pull time
+				a.lastPull = time.Now()
+				a.pulled = true
+
+				// If the pull was successful, return nil
 				return nil
+			} else {
+				log.Errorf("failed to pull artifact %s:%s from %s: %v\n", a.Repository, a.Reference, a.Registry, err)
 			}
 
-		// If the number of retries exceeds the max number of retries, return an error
-		// and cancel the context
-		case i := <-retries:
-			if i >= numRetries {
+			// If the pull failed, increment the retry count
+			retryCount++
+			if retryCount >= maxRetries {
+				// If the maximum number of retries has been reached, cancel the context and return the error
 				cancel()
-				return fmt.Errorf("failed to pull artifact %v:%v - max retries exceeded", a.Repository, a.Reference)
+				return fmt.Errorf("maximum number of retries reached: %w", err)
 			}
 
-		// If the artifact was pulled successfully, return nil
-		// and cancel the context
-		case err = <-done:
-			if err != nil {
-				return fmt.Errorf("failed to pull artifact %v:%v - %v", a.Repository, a.Reference, err)
-			}
-			cancel()
-			return nil
-
+			// If the pull failed and the maximum number of retries has not been reached, sleep for a while before retrying
+			time.Sleep(time.Second)
 		}
-
 	}
-
 }
 
-func (a *Artifact) pull(c context.Context, r chan int, done chan error) {
+func (a *Artifact) pull(c context.Context) error {
 
 	log.Infof("pulling artifact %s:%s from %s\n", a.Repository, a.Reference, a.Registry)
 
-	attempts := 1
-	for {
+	// create a temporary memory store
+	memoryStore := memory.New()
 
-		log.Infof("attempt %d: attempting pulling artifact %s:%s from %s\n", attempts, a.Repository, a.Reference, a.Registry)
-
-		// Send the number of attempts to the retries channel
-		r <- attempts
-
-		desc, err := oras.Copy(c, a.remote, a.Reference, a.fs, a.Reference, oras.DefaultCopyOptions)
-		if err != nil {
-			log.Errorf("%s:%s: attempt %d - %v\n", a.Repository, a.Reference, attempts)
-		}
-
-		// Check if the pull was successful
-		exists, err := a.fs.Exists(c, desc)
-		if exists && err == nil{
-
-			// Update the last pull time
-			a.lastPull = time.Now()
-			a.pulled = true
-
-			// TODO: replace with log message
-			log.Infof("pulled artifact %s:%s with %s from %s\n", a.Repository, a.Reference, desc.Digest, a.Registry)
-
-			// Send a nil value to the done channel to indicate the pull was successful
-			// TODO: should return the SHA of the pulled artifact and error
-			done <- nil
-			break // exit the loop
-		}
-
-		// For now, assume the pull was unsuccessful for the first 5 attempts
-		// and then successful on the 6th attempt
-		attempts++
-
-		// renew the file store
-		a.fs, err = file.New(a.Path)
-		defer a.fs.Close()
+	// Copy the artifact from the remote repository to the memory store
+	desc, err := oras.Copy(c, a.remote, a.Reference, memoryStore, a.Reference, oras.DefaultCopyOptions)
+	if err != nil {
+		return err
 	}
+
+	log.Debugf("copied artifact %s:%s to memory store\n", a.Repository, a.Reference)
+
+	// Create a new file store
+	a.fs, err = file.New(a.Path)
+	if err != nil {
+		return err
+	}
+
+	log.Debugf("created file store at %s\n", a.Path)
+	defer a.fs.Close()
+
+	// Copy the artifact from the memory store to the file store
+	desc, err = oras.Copy(c, memoryStore, a.Reference, a.fs, a.Reference, oras.DefaultCopyOptions)
+	if err != nil {
+		log.Errorf("%s:%s: %v\n", a.Repository, a.Reference, err)
+		return err
+	}
+
+	// Check if the copy from memory store to file store was successful
+	exists, err := a.fs.Exists(c, desc)
+	if exists && err == nil {
+		log.Infof("pulled artifact %s:%s with %s from %s\n", a.Repository, a.Reference, desc.Digest, a.Registry)
+		return nil
+	}
+
+	return err // return the error if the pull was not successful
 }
 
+// Parses and creates the remote repository based on the URL
 func (a *Artifact) Setup() error {
-
 
 	// Create a new remote repository
 	r, err := remote.NewRepository(a.URL)
@@ -179,6 +173,7 @@ func (a *Artifact) Setup() error {
 	return nil
 }
 
+// Prepare prepares the artifact for use
 func (a *Artifact) Prepare() error {
 
 	// Setup the artifact
@@ -186,17 +181,5 @@ func (a *Artifact) Prepare() error {
 		return err
 	}
 
-	log.Infof("creating file store for artifact %s:%s at %s\n", a.Repository, a.Reference, a.Path)
-
-	// Create a new file store
-	fs, err  := file.New(a.Path)
-	if err != nil {
-		// log.Infof("failed to create file store for artifact %s:%s at %s\n", a.Repository, a.Reference, a.Path)
-		return err
-	}
-	a.fs = fs
-	defer a.fs.Close()
-
-	return nil
-
+	return nil // return nil if the artifact was successfully prepared
 }
